4913d8d3b22eda7661a722a2b7c0a2bcf37a943f
Sergey <sergsnic@mail.ru>
2023-12-27 21:47:20 +0300

    add Task 63 time and date in hibernate

--- src/main/java/ru/sergsnic/springcourse/models/Person.java
+++ src/main/java/ru/sergsnic/springcourse/models/Person.java
@@ -1,10 +1,13 @@
 package ru.sergsnic.springcourse.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 
 @Entity
@@ -27,6 +30,13 @@ public class Person {
     @Email
     @Column(name = "email")
     private String email;
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dateOfBirth;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 
     public Person() {
 
@@ -68,6 +78,22 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
     @Override
